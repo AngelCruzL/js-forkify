76cac0ae928a91d92a75995f6405990e5c9c006e
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-13 14:21:52 -0500

    fix: arrow on next pagination button

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -4,6 +4,14 @@ import icons from 'url:../../img/icons.svg';
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', e => {
+      const btn = e.target.closest('.btn--inline');
+      console.log(btn);
+      handler();
+    });
+  }
+
   _generateMarkup() {
     const currPage = this._data.page;
     const numPages = Math.ceil(
@@ -28,15 +36,24 @@ class PaginationView extends View {
 
   _generateMarkupButton(currPage, isNextBtn = true) {
     return `
-        <button class="btn--inline pagination__btn--${
-          isNextBtn ? 'next' : 'prev'
-        }">
+        <button data-goto="${
+          isNextBtn ? currPage + 1 : currPage - 1
+        }" class="btn--inline pagination__btn--${isNextBtn ? 'next' : 'prev'}">
+        ${
+          isNextBtn
+            ? `
+          <span>Page ${currPage + 1}</span>
           <svg class="search__icon">
-            <use href="${icons}#icon-arrow-${
-      isNextBtn ? 'right' : 'left'
-    }"></use>
+            <use href="${icons}#icon-arrow-right"></use>
           </svg>
-          <span>Page ${isNextBtn ? currPage + 1 : currPage - 1}</span>
+          `
+            : `
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-left"></use>
+        </svg>
+        <span>Page ${currPage - 1}</span>
+        `
+        }
         </button>
       `;
   }
