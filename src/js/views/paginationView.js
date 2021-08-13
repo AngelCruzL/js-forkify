576a3b3089b6ce37c8ec9f0293c4cf3d71e28fdb
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-13 14:00:05 -0500

    feat: refactoring pagination button

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -9,48 +9,36 @@ class PaginationView extends View {
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
-    console.log(numPages);
 
-    if (currPage === 1 && numPages > 1) {
-      return `
-        <button class="btn--inline pagination__btn--next">
-          <span>Page ${currPage + 1}</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </button>
-      `;
-    }
+    if (currPage === 1 && numPages > 1)
+      return this._generateMarkupButton(currPage);
 
-    if (currPage === numPages && numPages > 1) {
-      return `
-        <button class="btn--inline pagination__btn--prev">
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-left"></use>
-          </svg>
-          <span>Page ${currPage - 1}</span>
-        </button>
-      `;
-    }
+    if (currPage === numPages && numPages > 1)
+      return this._generateMarkupButton(currPage, false);
 
     if (currPage < numPages) {
       return `
-        <button class="btn--inline pagination__btn--prev">
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-left"></use>
-          </svg>
-          <span>Page ${currPage - 1}</span>
-        </button>
-        <button class="btn--inline pagination__btn--next">
-          <span>Page ${currPage + 1}</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </button>
+      ${this._generateMarkupButton(currPage)}
+      ${this._generateMarkupButton(currPage, false)}
       `;
     }
 
     return '';
+  }
+
+  _generateMarkupButton(currPage, isNextBtn = true) {
+    return `
+        <button class="btn--inline pagination__btn--${
+          isNextBtn ? 'next' : 'prev'
+        }">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-${
+      isNextBtn ? 'right' : 'left'
+    }"></use>
+          </svg>
+          <span>Page ${isNextBtn ? currPage + 1 : currPage - 1}</span>
+        </button>
+      `;
   }
 }
 
